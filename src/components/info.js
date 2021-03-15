bb13e912312f9f9437599364dd600d9c69bac8fd
Abdurahman <yoabdurahman@hotmail.com>
2021-03-15 12:01:05 -0400

    Modified files

---
+++ src/components/info.js
@@ -0,0 +1,47 @@
+import React from "react"
+import Fade from "react-reveal/Fade"
+import data from "../yourdata"
+import loadable from '@loadable/component'
+
+const PDFResumeReader = loadable(() => import('./PDFResumeReader'))
+const PDFCoverLetterReader = loadable(() => import('./PDFCoverLetterReader'))
+
+const Info = () => {
+  return (
+    <div className="section" id="info">
+      <div className="container">
+        <div className="info-section">
+          <div className="work-wrapper">
+            <Fade bottom cascade>
+              <h1>Info</h1>
+            </Fade>
+            <Fade bottom cascade>
+              <h2>Resume</h2>
+            </Fade>
+            <p>
+              <PDFResumeReader />
+            </p>
+            <Fade bottom cascade>
+              <h2>Cover Letter</h2>
+            </Fade>
+              <PDFCoverLetterReader />
+            <Fade bottom cascade>
+              <h2>Statement Of Career Goal</h2>
+            </Fade>
+            <p>
+              {data.infoParaOne}
+              <br></br>
+              <br></br>
+              {data.infoParaTwo}
+              <br></br>
+              <br></br>
+              {data.infoParaThree}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Info
